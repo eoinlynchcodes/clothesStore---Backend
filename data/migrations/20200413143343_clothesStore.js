d7f70d5ff44bbc15ec802fec2caaeffa7e0c080a
eoinlynchcodes <eoin95lynch@gmail.com>
2020-04-13 16:16:23 +0100

    tables created

---
+++ data/migrations/20200413143343_clothesStore.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('users', table => {
+      table.increments('id').primary();
+      table.string('username', 128).unique().notNullable();
+      table.string('password', )
+  })
+  .createTable('clothesOnSale', table => {
+      table.increments('id');
+      table.string('seller').references('username').inTable('users');
+      table.string('brand').notNullable();
+      table.string('condition').notNullable();
+      table.string('itemType').notNullable();
+      table.varchar('size').notNullable();
+      table.string('description');
+      table.string('image').notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
