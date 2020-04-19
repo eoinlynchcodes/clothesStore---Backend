be0e872c129d0ac0d2378d326ff8063eb8ae1bca
eoinlynchcodes <eoin95lynch@gmail.com>
2020-04-19 15:29:29 +0100

    Push to Heroku.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const server = require('./api/server.js');
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
     console.log(`Ths server is running on port ${PORT}. This is from index.js`);
