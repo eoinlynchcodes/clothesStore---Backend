33129d646f382369809caf4a7fed457c67916429
eoinlynchcodes <eoin95lynch@gmail.com>
2020-04-19 15:51:24 +0100

    Push

--- clothesManagement/clothesRoutes.js
+++ clothesManagement/clothesRoutes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
 
-const clothesHelperFunctions = require('../clothesManagement/clothesHelpers');
+// const clothesHelperFunctions = require('../clothesManagement/clothesHelpers');
 
 router.get('/test', (req, res) => {
     res.status(200).json({message: 'The test route working works!'})
 })
 
-router.post('/addItem', (req, res) => {
-    const id = req.params.id;
-    const clothesItem = req.body;
-    clothesItem.userID = id;
-    clothesHelperFunctions.addClothes(clothesItem)
-    .then(response => {
-        res.status(201).json(response);
-    })
-    .catch(error => {
-        res.status(500).json('There is an error' + error );
-    });
-});
+// router.post('/addItem', (req, res) => {
+//     const id = req.params.id;
+//     const clothesItem = req.body;
+//     clothesItem.userID = id;
+//     clothesHelperFunctions.addClothes(clothesItem)
+//     .then(response => {
+//         res.status(201).json(response);
+//     })
+//     .catch(error => {
+//         res.status(500).json('There is an error' + error );
+//     });
+// });
 
 module.exports = router;
